7b90e0518e5fdd76b983b7280adeaa0f795b175f
Hemant Kumar <hmt364134@gmail.com>
2025-12-04 22:51:17 -0800

    add

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <p>
+        Now I can render any React component on any DOM node I want using
+        ReactDOM.render
+      </p>
     </div>
   );
 }
-
